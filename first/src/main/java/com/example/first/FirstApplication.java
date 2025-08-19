65cdb2640cdbe3764c7a60a6811883dff3fa94f5
SDS <SDS@sw-edu>
2025-08-19 13:55:42 +0900

    Second commit

--- first/src/main/java/com/example/first/FirstApplication.java
+++ first/src/main/java/com/example/first/FirstApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "di_spring")
-//@ComponentScan(basePackages = "springmvc")
 @ComponentScan(basePackages = "jpa")
 @EntityScan(basePackages = "jpa.entity")
+@EnableJpaRepositories(basePackages = "jpa.repository")
 public class FirstApplication {
 
 	public static void main(String[] args) {
--- first/src/main/java/jpa/repository/UsersRepository.java
+++ first/src/main/java/jpa/repository/UsersRepository.java
@@ -1,9 +1,11 @@
 package jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import jpa.entity.Users;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long>{
-
+	
 }
